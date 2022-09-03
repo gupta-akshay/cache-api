b2dbae53020dd94b5f77dea3f6ce9f34eb7e58f0
Akshay Gupta <akshaygupta0795@gmail.com>
2022-09-03 18:53:25 +0530

    add routes for cache

--- routes/cache.js
+++ routes/cache.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET cache listing. */
-router.get('/', function (req, res, next) {
+/* GET returns all stored keys in the cache */
+router.get('/', function (req, res, _next) {
+  res.send('respond with a resource');
+});
+
+/* GET returns the cached data for a given key */
+router.get('/:key', function (req, res, _next) {
+  res.send('respond with a resource');
+});
+
+/* POST creates the data for a given key */
+router.post('/:key', function (req, res, _next) {
+  res.send('respond with a resource');
+});
+
+/* PUT updates the data for a given key */
+router.put('/:key', function (req, res, _next) {
+  res.send('respond with a resource');
+});
+
+/* DELETE removes a given key from the cache */
+router.delete('/:key', function (req, res, _next) {
+  res.send('respond with a resource');
+});
+
+/* DELETE removes all keys from the cache */
+router.delete('/', function (req, res, _next) {
   res.send('respond with a resource');
 });
 
