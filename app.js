2ffd45063e0070a4ffdcfeca0c3db153d604abe7
Akshay Gupta <akshaygupta0795@gmail.com>
2022-09-04 12:36:58 +0530

    Add Mongoose + Add Model for CacheData

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
+require('dotenv').config({ path: '.env' });
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const Mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 const cacheRouter = require('./routes/cache');
 
 const app = express();
+
+Mongoose.connect(process.env.MONGODB_URL, { dbName: process.env.DB_NAME });
+Mongoose.connection.on('error', (e) => {
+  console.error('Error in MongoDB Connection');
+  console.error(e);
+  process.exit(1);
+});
 
 app.use(logger('dev'));
 app.use(express.json());
---
+++ models/CacheData.js
@@ -0,0 +1,25 @@
+const Mongoose = require('mongoose');
+const { getTimestampForExpiration } = require('../utils');
+const { Schema } = Mongoose;
+
+const defaultConfig = {
+  cacheSize: 100,
+  defaultTtl: 3600,
+};
+
+const cacheDataSchema = new Schema(
+  {
+    key: { type: String, unique: true },
+    value: String,
+    ttl: { type: Number, default: defaultConfig.defaultTtl },
+    expiresAt: {
+      type: Number,
+      default: getTimestampForExpiration(defaultConfig.defaultTtl),
+    },
+  },
+  { timestamps: true }
+);
+
+const CacheData = Mongoose.model('CacheData', cacheDataSchema);
+
+module.exports = CacheData;
